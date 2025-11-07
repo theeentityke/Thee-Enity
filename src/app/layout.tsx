f0191368567c1ef2465fe6ce84a114051431f180
Kareithi <kareithibrian1@gmail.com>
2025-11-07 08:19:15 +0300

    Navbar Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,11 +28,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Background */}
         <DynamicBackground />
 
-        {/* Navbar */}
-        <Navbar />
+        {/* Navbar - Hidden during loading using CSS */}
+        <div className={`transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
+          <Navbar />
+        </div>
 
         {/* Scroll Progress Indicator */}
-        <ScrollBar />
+        <div className={`transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
+          <ScrollBar />
+        </div>
 
         {/* Main Content */}
         <main className={`relative z-10 transition-opacity duration-500 ${isLoading ? 'opacity-0' : 'opacity-100'}`}>
